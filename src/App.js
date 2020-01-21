0f4da72a5a65cca0dba76a891911eb61424f9d80
rlaejrdud36 <rlaejrdud36@naver.com>
2020-01-21 13:57:53 +0900

    img & text animation.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Home from "./components/Pages/Home";
 const App = () => {
   return (
     <>
       <div>
-        <h1>안녕 리엑트</h1>
-        <h2>안녕 내이름은 홍길동이야.</h2>
-      </div>
-
-      <div>
-        <h1>우헿</h1>
-        <h2>아니야 아니아니아니야</h2>
-      </div>
-      <button id="b">r</button>
-
-      <div>
         <Header></Header>
       </div>
+
+      <div>
+        <Home></Home>
+      </div>
+      <div></div>
     </>
   );
 };
---
+++ src/components/Pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+import Typing from "react-typing-animation";
+
+import bg from "../../img/background1.jpg";
+
+const Img = styled.img`
+  width: 100vw;
+  height: 100%;
+`;
+const Home = () => {
+  return (
+    <>
+      <h1>여기는 home 페이지 입니다.</h1>
+      <h2>메롱이야.</h2>
+      <Img src={bg} />
+      <Typing loop="true" startDelay="1">
+        <span>이 페이지는 리윀트로 제작되었습니다.</span>
+        <Typing.Backspace count={30} startDelay="1000" />
+      </Typing>
+    </>
+  );
+};
+
+export default Home;
